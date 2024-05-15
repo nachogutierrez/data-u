80ae97e465f931a967b9c0e473d68b691f48df59
Nacho Gutierrez <nachogutierrez@Nachos-MBP.fritz.box>
2024-05-15 19:57:05 +0200

    frontend: refactor app page to simplify

--- frontend/app/(authenticated)/app/page.tsx
+++ frontend/app/(authenticated)/app/page.tsx
@@ -4,189 +4,33 @@ import Page from '@/components/Page';
 import { getSecret } from '@/secret-manager';
 import { runQuery as runNominatimQuery } from '@/lib/nominatim';
 import { getDataPointsLatest } from '@/db/bigquery/client';
-import { Filters, Operation, OrderByDirection, OrderByOption, PropertyType } from '@/db/bigquery/types';
 import DataPointsTable from '@/components/data/DataPointsTable';
 import DataPointsTableFilters from '@/components/data/DataPointsTableFilters';
+import { validateSearchParams, extractFilters } from '@/lib/validation';
 
 type AppPageProps = {
   searchParams?: { [key: string]: string | string[] | undefined }
 }
 
-const integerRegex = /^[1-9][0-9]*|0$/
-const floatRegex = /^[0-9]\.[0-9]{2}$/
-const locationQueryRegex = /^[a-zA-Z ,]+$/
-
-function validateIsIntegerInRange(name: string, value: string, min: number|undefined, max: number|undefined) {
-  if (value === undefined) {
-    return
-  }
-
-  if (!value.match(integerRegex)) {
-    throw new Error(`Expected '${name}' to be an integer. Got '${value}'`)
-  }
-  const intValue = Number(value)
-
-  if (min && intValue < min) {
-    throw new Error(`Expected '${name}' to be at least ${min}. Got '${value}'`)
-  }
-
-  if (max &&intValue > max) {
-    throw new Error(`Expected '${name}' to be at most ${max}. Got '${value}'`)
-  }
-}
-
-function validateIsFloatInRange(name: string, value: string, min: number|undefined, max: number|undefined) {
-  if (value === undefined) {
-    return
-  }
-
-  if (!value.match(floatRegex)) {
-    throw new Error(`Expected '${name}' to be a float. Got '${value}'`)
-  }
-  const floatValue = parseFloat(value)
-
-  if (min && floatValue < min) {
-    throw new Error(`Expected '${name}' to be at least ${min}. Got '${value}'`)
-  }
-
-  if (max && floatValue > max) {
-    throw new Error(`Expected '${name}' to be at most ${max}. Got '${value}'`)
-  }
-}
-
-function validateIntegerDifference(nameA: string, nameB: string, valueA: string, valueB: string) {
-  if (valueA === undefined || valueB === undefined) {
-    return
-  }
-
-  const a = Number(valueA)
-  const b = Number(valueB)
-
-  if (a > b) {
-    throw new Error(`Expected ${nameA} to be lower than or equal to ${nameB}`)
-  }
-}
-
-function validateIsEnum(name: string, value: string, enumType: any) {
-  if (value === undefined) {
-    return
-  }
-
-  const num = Number(value)
-
-  if (!Object.values(enumType).includes(num)) {
-    throw new Error(`Invalid value '${value}' for ${name}`)
-  }
-}
-
-function validateLocationQuery(name: string, value: string) {
-  if (value === undefined) {
-    return
-  }
-
-  const decoded = Buffer.from(value, 'base64').toString('utf-8')
-
-  if (!decoded.match(locationQueryRegex)) {
-    throw new Error(`Expected ${name} to match regex ${locationQueryRegex} make sure ${name} is base64-encoded.`)
-  }
-
-  if (decoded.length === 0) {
-    throw new Error(`Location query can't be empty.`)
-  }
-
-  if (decoded.length > 100) {
-    throw new Error(`Location query can't be more than 100 characters long. Got ${decoded.length} characters.`)
-  }
-}
-
 export default async function AppPage(props: AppPageProps) {
+  // Validate search params
+  validateSearchParams(props.searchParams);
 
-  // TODO: handle errors more gracefully
+  // Fetch secrets
+  const googleMapsApiKey = await getSecret('GOOGLE_MAPS_API_KEY');
 
-  validateIsIntegerInRange('ps', props.searchParams?.ps as string, 1, 100)
-  validateIsIntegerInRange('p', props.searchParams?.p as string, 0, 500000)
-  validateIsIntegerInRange('minp', props.searchParams?.minp as string, 1, 999999999)
-  validateIsIntegerInRange('maxp', props.searchParams?.maxp as string, 1, 999999999)
-  validateIsIntegerInRange('minpsm', props.searchParams?.minpsm as string, 1, 999999)
-  validateIsIntegerInRange('maxpsm', props.searchParams?.maxpsm as string, 1, 999999)
-  validateIsIntegerInRange('mindc', props.searchParams?.mindc as string, 1, 999999999)
-  validateIsIntegerInRange('maxdc', props.searchParams?.maxdc as string, 1, 999999999)
-  validateIsIntegerInRange('obo', props.searchParams?.obo as string, 0, undefined) // Enum values
-  validateIsIntegerInRange('obd', props.searchParams?.obd as string, 0, undefined) // Enum values
-  validateIsIntegerInRange('minpdo', props.searchParams?.mindc as string, 0, undefined)
-  validateIsIntegerInRange('maxpdo', props.searchParams?.maxdc as string, 0, undefined)
-  validateIsFloatInRange('minpde', props.searchParams?.mindc as string, 0, undefined)
-  validateIsFloatInRange('maxpde', props.searchParams?.maxdc as string, 0, undefined)
-
-  validateIntegerDifference('minp', 'maxp', props.searchParams?.minp as string, props.searchParams?.maxp as string)
-  validateIntegerDifference('minpsm', 'maxpsm', props.searchParams?.minpsm as string, props.searchParams?.maxpsm as string)
-  validateIntegerDifference('mindc', 'maxdc', props.searchParams?.mindc as string, props.searchParams?.maxdc as string)
-
-  validateIsEnum('obo', props.searchParams?.obo as string, OrderByOption)
-  validateIsEnum('obd', props.searchParams?.obd as string, OrderByDirection)
-  validateIsEnum('t', props.searchParams?.t as string, PropertyType)
-  validateIsEnum('op', props.searchParams?.op as string, Operation)
-
-  validateLocationQuery('q', props.searchParams?.q as string)
-
-  const googleMapsApiKey = await getSecret('GOOGLE_MAPS_API_KEY')
-
-  const locationQueryBase64 = props.searchParams?.q as string
-  const pageSize = Number(props.searchParams?.ps) || 100
-  const pageNumber = Number(props.searchParams?.p) || 0
-  const minPrice = Number(props.searchParams?.minp)
-  const maxPrice = Number(props.searchParams?.maxp)
-  const minPriceM2 = Number(props.searchParams?.minpsm)
-  const maxPriceM2 = Number(props.searchParams?.maxpsm)
-  const minDimensionCovered = Number(props.searchParams?.mindc)
-  const maxDimensionCovered = Number(props.searchParams?.maxdc)
-  const minPriceDowns = Number(props.searchParams?.minpdo)
-  const maxPriceDowns = Number(props.searchParams?.maxpdo)
-  const minPriceDelta = parseFloat(props.searchParams?.minpde as any)
-  const maxPriceDelta = parseFloat(props.searchParams?.maxpde as any)
-  const orderByOption = OrderByOption[OrderByOption[Number(props.searchParams?.obo)] as keyof typeof OrderByOption] || OrderByOption.PRICE_M2
-  const orderByDirection = OrderByDirection[OrderByDirection[Number(props.searchParams?.obo)] as keyof typeof OrderByDirection] || OrderByDirection.ASC
-  const type = PropertyType[PropertyType[Number(props.searchParams?.t)] as keyof typeof PropertyType]
-  const operation = Operation[Operation[Number(props.searchParams?.op)] as keyof typeof Operation]
-  let mapPolygon = undefined
-
-  if (locationQueryBase64 !== undefined) {
-    const decodedLocationQuery = Buffer.from(locationQueryBase64, 'base64').toString('utf-8')
-    const { polygon, boundingbox, center } = await runNominatimQuery(decodedLocationQuery)
-
-    mapPolygon = polygon
-  }
-
-  const maybe = (predicate: any, name: string, value: any) => predicate() ? ({ [name]: value }) : ({})
-
-  const filters: Filters = {
-    ...maybe(() => mapPolygon, 'polygon', mapPolygon),
-    ...maybe(() => minPrice || maxPrice, 'price', { ...maybe(() => minPrice, 'min', minPrice), ...maybe(() => maxPrice, 'max', maxPrice) }),
-    ...maybe(() => minPriceDowns || maxPriceDowns, 'priceDowns', { ...maybe(() => minPriceDowns, 'min', minPriceDowns), ...maybe(() => maxPriceDowns, 'max', maxPriceDowns) }),
-    ...maybe(() => minPriceDelta || maxPriceDelta, 'priceDelta', { ...maybe(() => minPriceDelta, 'min', minPriceDelta), ...maybe(() => maxPriceDelta, 'max', maxPriceDelta) }),
-    ...maybe(() => minPriceM2 || maxPriceM2, 'priceM2', { ...maybe(() => minPriceM2, 'min', minPriceM2), ...maybe(() => maxPriceM2, 'max', maxPriceM2) }),
-    ...maybe(() => minDimensionCovered || maxDimensionCovered, 'dimensionCovered', { ...maybe(() => minDimensionCovered, 'min', minDimensionCovered), ...maybe(() => maxDimensionCovered, 'max', maxDimensionCovered) }),
-    ...maybe(() => type !== undefined, 'type', type),
-    ...maybe(() => operation !== undefined, 'operation', operation),
-    pagination: { pageNumber, pageSize },
-    sort: {
-      option: orderByOption,
-      direction: orderByDirection
-    }
-  }
+  // Extract filters from search params
+  const filters = await extractFilters(props.searchParams);
 
   console.log({ filters });
-  
 
-  // TODO: enable data
-  const data = await getDataPointsLatest(filters)
-
-  // await new Promise(resolve => setTimeout(resolve, 1000))
+  // Fetch data points
+  const data = await getDataPointsLatest(filters);
 
   return (
     <Page>
       <DataPointsTableFilters googleMapsApiKey={googleMapsApiKey} />
       <DataPointsTable data={data}></DataPointsTable>
     </Page>
-  )
+  );
 }
---
+++ frontend/lib/validation.ts
@@ -0,0 +1,154 @@
+import { Filters, Operation, OrderByDirection, OrderByOption, PropertyType } from '@/db/bigquery/types';
+import { runQuery as runNominatimQuery } from '@/lib/nominatim';
+
+const integerRegex = /^[1-9][0-9]*|0$/;
+const floatRegex = /^[0-9]\.[0-9]{2}$/;
+const locationQueryRegex = /^[a-zA-Z ,]+$/;
+
+function validateIsIntegerInRange(name: string, value: string, min?: number, max?: number) {
+  if (value === undefined) return;
+
+  if (!value.match(integerRegex)) {
+    throw new Error(`Expected '${name}' to be an integer. Got '${value}'`);
+  }
+  const intValue = Number(value);
+
+  if (min !== undefined && intValue < min) {
+    throw new Error(`Expected '${name}' to be at least ${min}. Got '${value}'`);
+  }
+
+  if (max !== undefined && intValue > max) {
+    throw new Error(`Expected '${name}' to be at most ${max}. Got '${value}'`);
+  }
+}
+
+function validateIsFloatInRange(name: string, value: string, min?: number, max?: number) {
+  if (value === undefined) return;
+
+  if (!value.match(floatRegex)) {
+    throw new Error(`Expected '${name}' to be a float. Got '${value}'`);
+  }
+  const floatValue = parseFloat(value);
+
+  if (min !== undefined && floatValue < min) {
+    throw new Error(`Expected '${name}' to be at least ${min}. Got '${value}'`);
+  }
+
+  if (max !== undefined && floatValue > max) {
+    throw new Error(`Expected '${name}' to be at most ${max}. Got '${value}'`);
+  }
+}
+
+function validateIntegerDifference(nameA: string, nameB: string, valueA: string, valueB: string) {
+  if (valueA === undefined || valueB === undefined) return;
+
+  const a = Number(valueA);
+  const b = Number(valueB);
+
+  if (a > b) {
+    throw new Error(`Expected ${nameA} to be lower than or equal to ${nameB}`);
+  }
+}
+
+function validateIsEnum(name: string, value: string, enumType: any) {
+  if (value === undefined) return;
+
+  const num = Number(value);
+
+  if (!Object.values(enumType).includes(num)) {
+    throw new Error(`Invalid value '${value}' for ${name}`);
+  }
+}
+
+function validateLocationQuery(name: string, value: string) {
+  if (value === undefined) return;
+
+  const decoded = Buffer.from(value, 'base64').toString('utf-8');
+
+  if (!decoded.match(locationQueryRegex)) {
+    throw new Error(`Expected ${name} to match regex ${locationQueryRegex}. Make sure ${name} is base64-encoded.`);
+  }
+
+  if (decoded.length === 0) {
+    throw new Error(`Location query can't be empty.`);
+  }
+
+  if (decoded.length > 100) {
+    throw new Error(`Location query can't be more than 100 characters long. Got ${decoded.length} characters.`);
+  }
+}
+
+export function validateSearchParams(params: { [key: string]: string | string[] | undefined } | undefined) {
+  validateIsIntegerInRange('ps', params?.ps as string, 1, 100);
+  validateIsIntegerInRange('p', params?.p as string, 0, 500000);
+  validateIsIntegerInRange('minp', params?.minp as string, 1, 999999999);
+  validateIsIntegerInRange('maxp', params?.maxp as string, 1, 999999999);
+  validateIsIntegerInRange('minpsm', params?.minpsm as string, 1, 999999);
+  validateIsIntegerInRange('maxpsm', params?.maxpsm as string, 1, 999999);
+  validateIsIntegerInRange('mindc', params?.mindc as string, 1, 999999999);
+  validateIsIntegerInRange('maxdc', params?.maxdc as string, 1, 999999999);
+  validateIsIntegerInRange('obo', params?.obo as string, 0, undefined); // Enum values
+  validateIsIntegerInRange('obd', params?.obd as string, 0, undefined); // Enum values
+  validateIsIntegerInRange('minpdo', params?.minpdo as string, 0, undefined);
+  validateIsIntegerInRange('maxpdo', params?.maxpdo as string, 0, undefined);
+  validateIsFloatInRange('minpde', params?.minpde as string, 0, undefined);
+  validateIsFloatInRange('maxpde', params?.maxpde as string, 0, undefined);
+
+  validateIntegerDifference('minp', 'maxp', params?.minp as string, params?.maxp as string);
+  validateIntegerDifference('minpsm', 'maxpsm', params?.minpsm as string, params?.maxpsm as string);
+  validateIntegerDifference('mindc', 'maxdc', params?.mindc as string, params?.maxdc as string);
+
+  validateIsEnum('obo', params?.obo as string, OrderByOption);
+  validateIsEnum('obd', params?.obd as string, OrderByDirection);
+  validateIsEnum('t', params?.t as string, PropertyType);
+  validateIsEnum('op', params?.op as string, Operation);
+
+  validateLocationQuery('q', params?.q as string);
+}
+
+export async function extractFilters(params: { [key: string]: string | string[] | undefined } | undefined): Promise<Filters> {
+  const locationQueryBase64 = params?.q as string;
+  const pageSize = Number(params?.ps) || 100;
+  const pageNumber = Number(params?.p) || 0;
+  const minPrice = Number(params?.minp);
+  const maxPrice = Number(params?.maxp);
+  const minPriceM2 = Number(params?.minpsm);
+  const maxPriceM2 = Number(params?.maxpsm);
+  const minDimensionCovered = Number(params?.mindc);
+  const maxDimensionCovered = Number(params?.maxdc);
+  const minPriceDowns = Number(params?.minpdo);
+  const maxPriceDowns = Number(params?.maxpdo);
+  const minPriceDelta = parseFloat(params?.minpde as any);
+  const maxPriceDelta = parseFloat(params?.maxpde as any);
+  const orderByOption = OrderByOption[OrderByOption[Number(params?.obo)] as keyof typeof OrderByOption] || OrderByOption.PRICE_M2;
+  const orderByDirection = OrderByDirection[OrderByDirection[Number(params?.obo)] as keyof typeof OrderByDirection] || OrderByDirection.ASC;
+  const type = PropertyType[PropertyType[Number(params?.t)] as keyof typeof PropertyType];
+  const operation = Operation[Operation[Number(params?.op)] as keyof typeof Operation];
+  let mapPolygon = undefined;
+
+  if (locationQueryBase64 !== undefined) {
+    const decodedLocationQuery = Buffer.from(locationQueryBase64, 'base64').toString('utf-8');
+    const { polygon } = await runNominatimQuery(decodedLocationQuery);
+    mapPolygon = polygon;
+  }
+
+  const maybe = (predicate: any, name: string, value: any) => (predicate() ? { [name]: value } : {});
+
+  const filters: Filters = {
+    ...maybe(() => mapPolygon, 'polygon', mapPolygon),
+    ...maybe(() => minPrice || maxPrice, 'price', { ...maybe(() => minPrice, 'min', minPrice), ...maybe(() => maxPrice, 'max', maxPrice) }),
+    ...maybe(() => minPriceDowns || maxPriceDowns, 'priceDowns', { ...maybe(() => minPriceDowns, 'min', minPriceDowns), ...maybe(() => maxPriceDowns, 'max', maxPriceDowns) }),
+    ...maybe(() => minPriceDelta || maxPriceDelta, 'priceDelta', { ...maybe(() => minPriceDelta, 'min', minPriceDelta), ...maybe(() => maxPriceDelta, 'max', maxPriceDelta) }),
+    ...maybe(() => minPriceM2 || maxPriceM2, 'priceM2', { ...maybe(() => minPriceM2, 'min', minPriceM2), ...maybe(() => maxPriceM2, 'max', maxPriceM2) }),
+    ...maybe(() => minDimensionCovered || maxDimensionCovered, 'dimensionCovered', { ...maybe(() => minDimensionCovered, 'min', minDimensionCovered), ...maybe(() => maxDimensionCovered, 'max', maxDimensionCovered) }),
+    ...maybe(() => type !== undefined, 'type', type),
+    ...maybe(() => operation !== undefined, 'operation', operation),
+    pagination: { pageNumber, pageSize },
+    sort: {
+      option: orderByOption,
+      direction: orderByDirection,
+    },
+  };
+
+  return filters;
+}
